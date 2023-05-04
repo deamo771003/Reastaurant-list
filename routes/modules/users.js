f5ee9b295fb0683b24bf362d365eaf97a10688a9
JimmyLin <disillusioned2077@gmail.com>
2023-05-04 16:38:20 +0800

    feat: cancel user name required

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -22,11 +22,11 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   const errors = []
-  if (!name || !email || !password || !confirmPassword) {
-    errors.push({ message: '所有欄位都是必填。' })
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: 'email、password、confirmPassword 是必填。' })
   }
   if (password !== confirmPassword) {
-    errors.push({ message: '密碼與確認密碼不符。' })
+    errors.push({ message: 'password 與 confirmPassword 不符。' })
   }
   if (errors.length) {
     return res.render('register', { errors, name, email, password, confirmPassword })
